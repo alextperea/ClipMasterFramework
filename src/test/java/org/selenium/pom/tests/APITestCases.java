cecaaeaddb6f43b7c9770b125be6e831afb36bb3
Alejandro Topete <alextopeteperea@gmail.com>
2024-01-20 23:04:18 -0600

    Adding some allure features to the tests

--- src/test/java/org/selenium/pom/tests/APITestCases.java
+++ src/test/java/org/selenium/pom/tests/APITestCases.java
@@ -1,6 +1,7 @@
 package org.selenium.pom.tests;
 
 
+import io.qameta.allure.Description;
 import io.restassured.response.Response;
 import org.selenium.pom.apis.catalog.CreateSingleProduct;
 import org.selenium.pom.apis.catalog.DeleteProduct;
@@ -10,7 +11,8 @@ import org.testng.annotations.Test;
 import java.io.IOException;
 
 public class APITestCases {
-    @Test
+    @Description("This endpoint should login and get a 200OK HTTP code as well as store the access token fo future uses")
+    @Test(description = "Should be able to login and get access token")
     public void LoginExec() throws IOException {
         Login login = new Login();
 
@@ -18,7 +20,8 @@ public class APITestCases {
                             login.login();
                             login.validateLoginResponse(response);
     }
-    @Test
+    @Description("This test should be able to Login, store an access_token and then get the categories in the Catalog")
+    @Test(description = "Get Catalog Categories")
     public void CategoriesExec() throws IOException {
         Login login = new Login();
         Response responseLogin =
@@ -32,7 +35,8 @@ public class APITestCases {
                                         getCategories.validateCategoriesResponse(responseGetCategories);
 
     }
-    @Test
+    @Description("This test should be able to Login, create a single product in the catalog and then delete that same product | this follows CRUD pattern")
+    @Test(description = "Make a CRUD for Catalog single product")
     public void CRUDForCatalog() throws IOException {
         Login login = new Login();
         Response responseLogin =
